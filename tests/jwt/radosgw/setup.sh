fc6ae202a7266d753e2075c0cbd6e70451ef047e
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-02-07 15:12:03 -0800

    Fix term
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/jwt/radosgw/setup.sh
+++ tests/jwt/radosgw/setup.sh
@@ -19,20 +19,20 @@ kubectl wait --for=jsonpath='{.status.readyReplicas}'=1 deploy/rook-ceph-tools -
 kubectl wait --for=create deploy/rook-ceph-rgw-ceph-objectstore-a -n rook-ceph --timeout=300s
 kubectl wait --for=jsonpath='{.status.readyReplicas}'=1 deploy/rook-ceph-rgw-ceph-objectstore-a -n rook-ceph --timeout=300s
 
-kubectl exec -it -n rook-ceph deploy/rook-ceph-tools -- bash -c 'radosgw-admin --rgw-zone=ceph-objectstore user create --uid=oidc-admin --display-name="OIDC Admin" --access-key=oidc-admin --secret-key=test1234'
-kubectl exec -it -n rook-ceph deploy/rook-ceph-tools -- bash -c 'radosgw-admin --rgw-zone=ceph-objectstore caps add --uid=oidc-admin --caps="oidc-provider=*"'
-kubectl exec -it -n rook-ceph deploy/rook-ceph-tools -- bash -c 'radosgw-admin --rgw-zone=ceph-objectstore caps add --uid=oidc-admin --caps="roles=*"'
+kubectl exec -i -n rook-ceph deploy/rook-ceph-tools -- bash -c 'radosgw-admin --rgw-zone=ceph-objectstore user create --uid=oidc-admin --display-name="OIDC Admin" --access-key=oidc-admin --secret-key=test1234'
+kubectl exec -i -n rook-ceph deploy/rook-ceph-tools -- bash -c 'radosgw-admin --rgw-zone=ceph-objectstore caps add --uid=oidc-admin --caps="oidc-provider=*"'
+kubectl exec -i -n rook-ceph deploy/rook-ceph-tools -- bash -c 'radosgw-admin --rgw-zone=ceph-objectstore caps add --uid=oidc-admin --caps="roles=*"'
 
 FOO=N5u4qNh6K8Tk1JfK
-kubectl exec -it -n rook-ceph deploy/rook-ceph-tools -- bash -c "ceph config set client.rgw.ceph.objectstore 'rgw sts key' $FOO"
-kubectl exec -it -n rook-ceph deploy/rook-ceph-tools -- bash -c "ceph config set client.rgw.ceph.objectstore 'rgw s3 auth use sts' true"
-kubectl exec -it -n rook-ceph deploy/rook-ceph-tools -- bash -c 'ceph config set client.rgw rgw_verify_ssl false'
+kubectl exec -i -n rook-ceph deploy/rook-ceph-tools -- bash -c "ceph config set client.rgw.ceph.objectstore 'rgw sts key' $FOO"
+kubectl exec -i -n rook-ceph deploy/rook-ceph-tools -- bash -c "ceph config set client.rgw.ceph.objectstore 'rgw s3 auth use sts' true"
+kubectl exec -i -n rook-ceph deploy/rook-ceph-tools -- bash -c 'ceph config set client.rgw rgw_verify_ssl false'
 
-kubectl exec -it -n rook-ceph deploy/rook-ceph-tools -- bash -c 'ceph config set client.rgw.ceph.objectstore.a debug_rgw 20'
-kubectl exec -it -n rook-ceph deploy/rook-ceph-tools -- bash -c '#ceph config set client.rgw.ceph.objectstore.a debug_ms 1'
+kubectl exec -i -n rook-ceph deploy/rook-ceph-tools -- bash -c 'ceph config set client.rgw.ceph.objectstore.a debug_rgw 20'
+kubectl exec -i -n rook-ceph deploy/rook-ceph-tools -- bash -c '#ceph config set client.rgw.ceph.objectstore.a debug_ms 1'
 
 kubectl rollout restart deploy/rook-ceph-rgw-ceph-objectstore-a -n rook-ceph
 
 kubectl wait --for=jsonpath='{.status.readyReplicas}'=1 deploy/rook-ceph-rgw-ceph-objectstore-a -n rook-ceph --timeout=300s
 
-kubectl exec -it admin-0 -- bash -c "~/setup.sh"
+kubectl exec -i admin-0 -- bash -c "~/setup.sh"
